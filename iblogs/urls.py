1d4f6dbbfc9442f4abe1c3de194aff5d418619d9
Atul Rathod <130772262+atulr2200@users.noreply.github.com>
2023-11-02 12:48:25 +0530

    Update urls.py

--- iblogs/urls.py
+++ iblogs/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('blog.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
