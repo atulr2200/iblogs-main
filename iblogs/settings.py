6e9aa1ec57704e1c091f172cb518a3ba18de3574
Atul Rathod <atulr2200@gmail.com>
2023-11-03 14:13:23 +0530

    first commit

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4b5=kb%i^!ujji594f%cw6a=%5o#oq_dq1ue&zhzwkh=hr6jkv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['creativeatul.tech','creativeatul.onrender.com','*']
 
 # Application definition
 
