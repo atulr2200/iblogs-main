80845de15d0ae8f45f3c73565b5eac04f019253c
Atul Rathod <atulr2200@gmail.com>
2023-11-06 12:27:53 +0530

    push render

--- build.sh
+++
@@ -1,6 +0,0 @@
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
--- iblogs/urls.py
+++ iblogs/urls.py
@@ -21,16 +21,8 @@ from django.conf import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('blog.urls'))
-<<<<<<< HEAD
-              ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-              ] 
+]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
->>>>>>> 720e330a01922b4a21d44d5bd811ea95ccdaf373
