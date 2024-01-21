dbe0d5ebbef5724c9f07262050f7413efc2d558e
Atul Rathod <atulr2200@gmail.com>
2024-01-21 17:20:11 +0530

    first commit

--- iblogs/urls.py
+++ iblogs/urls.py
@@ -26,5 +26,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
